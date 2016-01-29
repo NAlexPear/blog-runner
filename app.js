66ee1f15b8c5d8576511d4d300fee644a8a024bb
Alex Pearson <alex@alexpear.com>
2016-01-28 22:37:27 -0600

    Reorganize build command for module.exports

--- app.js
+++ app.js
@@ -26,52 +26,6 @@ function files(source){
   return paths;
 }
 
-//sets up directory structure in _site
-function build(source){
-  const paths = files(source);
-  const view = includes(paths.includes);
-
-  //build _site directory, unless it already exists
-  const sitePath = paths.site;
-  fs.mkdir(sitePath, (err) => {
-    if (err) {
-      if (err.code === 'EEXIST') console.log('the _site folder already exists, building some new children...');
-      else console.log(err);
-    } else console.log('_site directory initialized')
-  });
-
-  //get array of all posts in _posts directory using glob module
-  //then create a new set of directories according to the post title
-  const postsPath = paths.posts;
-  glob(postsPath + '/**/*.md', (err, files) => {
-    if (err) console.log(err);
-    else files.forEach(doc => {
-      const filename = doc.substring(postsPath.length + 1);
-      const post = postParse(filename);
-
-      //run down the directory structure rabbit hole for each post
-      postDir(sitePath, post, 'year');
-      postDir(sitePath, post, 'month');
-      postDir(sitePath, post, 'day');
-      postDir(sitePath, post, 'title');
-
-      //move markdown and template (view) data to index.html file at root of post directories
-      reader(doc, (data) => {
-        const html = marked(data);
-        const path = sitePath + '/' + post.year + '/' + post.month + '/' + post.day + '/' + post.title;
-        writer(paths.layouts, view, path, html, 'posts');
-      });
-    });
-  });
-
-  //build landing page (index.html) using same writer function as for posts, without needing to read markdown
-  writer(paths.layouts, view, sitePath, '', 'landing');
-
-}
-
-//quick test on the example directory
-build('example');
-
 //parse the filename and output an object of year, month, day, and title
 //filename must have the format YYYY-MM-DD-title-with-hyphens.*
 function postParse(filename){
@@ -139,4 +93,47 @@ function layout(layoutsPath, view, data, type){
   let layout = fs.readFileSync(layoutsPath + '/' + type + '.html', 'utf8');
 
   return mustache.render(layout, view);
+}
+module.exports = {
+  //sets up directory structure in _site
+  build(source){
+    const paths = files(source);
+    const view = includes(paths.includes);
+
+    //build _site directory, unless it already exists
+    const sitePath = paths.site;
+    fs.mkdir(sitePath, (err) => {
+      if (err) {
+        if (err.code === 'EEXIST') console.log('the _site folder already exists, building some new children...');
+        else console.log(err);
+      } else console.log('_site directory initialized')
+    });
+
+    //get array of all posts in _posts directory using glob module
+    //then create a new set of directories according to the post title
+    const postsPath = paths.posts;
+    glob(postsPath + '/**/*.md', (err, files) => {
+      if (err) console.log(err);
+      else files.forEach(doc => {
+        const filename = doc.substring(postsPath.length + 1);
+        const post = postParse(filename);
+
+        //run down the directory structure rabbit hole for each post
+        postDir(sitePath, post, 'year');
+        postDir(sitePath, post, 'month');
+        postDir(sitePath, post, 'day');
+        postDir(sitePath, post, 'title');
+
+        //move markdown and template (view) data to index.html file at root of post directories
+        reader(doc, (data) => {
+          const html = marked(data);
+          const path = sitePath + '/' + post.year + '/' + post.month + '/' + post.day + '/' + post.title;
+          writer(paths.layouts, view, path, html, 'posts');
+        });
+      });
+    });
+
+    //build landing page (index.html) using same writer function as for posts, without needing to read markdown
+    writer(paths.layouts, view, sitePath, '', 'landing');
+  }
 }
