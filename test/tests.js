eddf01f00333fc72b1b7a436d59ab9d9998506b6
Alex Pearson <alex@alexpear.com>
2016-01-29 18:07:55 -0600

    Set up first successful dummy test

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,15 @@
 'use strict';
 
 const chai = require('chai');
+const assert = require('chai').assert;
+const Blog = require('../lib/app.js');
+const Helpers = require('../lib/helpers.js');
+
+
+const foo = 'bar';
+
+describe('the first test', function() {
+  it('should pass if foo is a string', function(){
+    assert.typeOf(foo, 'string', 'foo is a string!');    
+  });
+});
