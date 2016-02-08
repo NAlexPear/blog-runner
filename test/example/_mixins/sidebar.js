6dbe44e54dfa3ca011009dbc95b24e15591d64ff
Alex Pearson <alex@alexpear.com>
2016-02-07 19:31:15 -0600

    Add month and year arrays for sidebar.html

--- test/example/_mixins/sidebar.js
+++ test/example/_mixins/sidebar.js
@@ -12,11 +12,59 @@ const Index = Blog.index(__dirname + '/../../example');
 module.exports = {
     output(){
       let html = '<div class="sidebar"><ul>';
+      let years = [];
+      let months = [];
 
       Index.forEach(post => {
+        let month = '';
+        switch (post.month) {
+          case '01':
+            month = 'January';
+            break;
+          case '02':
+            month = 'February';
+            break;
+          case '03':
+            month = "March";
+            break;
+          case '04':
+            month = "April";
+            break;
+          case '05':
+            month = "May";
+            break;
+          case '06':
+            month = 'June';
+            break;
+          case '07':
+            month = 'July';
+            break;
+          case '08':
+            month = 'August';
+            break;
+          case '09':
+            month = 'September';
+            break;
+          case '10':
+            month = 'October';
+            break;
+          case '11':
+            month = 'November';
+            break;
+          case '12':
+            month = 'December';
+            break;
+        }
+
+        if (years.indexOf(post.year) === -1) years.push(post.year);
+        if (months.indexOf(month) === -1) months.push(month);
         const path = post.year + '/' + post.month + '/' + post.day + '/' + post.title;
         html+= '<li><a href="' + path + '">' + post.title + '</a></li>';
       });
+
+
+      console.log(years);
+      console.log(months);
 
       html+= '</ul></div>';
       return html;
